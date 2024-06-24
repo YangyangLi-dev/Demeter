b054a3fba6fec045f6acbd21b4e87ca3d0a33fff
YangyangLi <yangyangli@penguin-benefits.com>
2024-06-24 10:08:09 -0400

    Initial commit

---
+++ src/main/java/com/platform/demeter/DemeterApplication.java
@@ -0,0 +1,13 @@
+package com.platform.demeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/demeter/DemeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.demeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
